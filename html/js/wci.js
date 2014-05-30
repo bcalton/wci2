1031b104adf2384586afbe161e0e90389b2dc370
Ben Calton <bac@pml.ac.uk>
2014-05-30 16:37:50 +0000

    bac: new shinny wci site
    git-svn-id: https://rsg.pml.ac.uk/intranet/svnroots/rsg/website/trunk@37831 d9bbaac8-6a0f-0410-ba56-dc3909ed4b5b

---
+++ html/js/wci.js
@@ -0,0 +1,19 @@
+
+function goToByScroll(id){
+      // Remove "link" from the ID
+    id = id.replace("link", "");
+      // Scroll
+    $('html,body').animate({
+        scrollTop: $("#"+id).offset().top -150},
+        'slow');
+}
+
+$("ul.navbar-nav > li > a").click(function(e) { 
+	e.preventDefault(); 
+   goToByScroll($(this).attr("id"));           
+});
+
+$(".top-link").click(function(e) { 
+	e.preventDefault(); 
+   goToByScroll('banner');           
+});
